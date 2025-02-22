12c042470b8720d712671bb70071bf16d0be03d9
Terillian <terillian@yahoo.com>
2025-02-21 17:20:46 -0800

    nav menu finalization

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function toggleMobileMenu(menu) {
+function toggleMobileMenu() {
+    let menu = document.querySelector('.mobile-menu');
+    let hamburger = document.querySelector('#hamburger-icon');
+
     menu.classList.toggle('open');
+    hamburger.classList.toggle('open');  // Ensures the animation on the hamburger
 }
