d65b29504b0979944b00e0fd2fe8d9e8fda846bf
Terillian <terillian33@gmail.com>
2025-02-28 12:32:26 -0800

    add modals

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ** Mobile Menu **
 function toggleMobileMenu() {
     let menu = document.querySelector('.mobile-menu');
     let hamburger = document.querySelector('#hamburger-icon');
@@ -6,10 +7,33 @@ function toggleMobileMenu() {
     hamburger.classList.toggle('open');  
 }
 
-function openInNewTab(url) {
-    window.open(url, '_blank').focus();
-  }
+// ** Modals **
+  function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
 
-  function navigateToURL(url) {
-    window.location.href = url;
-  }
+// Function to close the modal
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    var modals = document.getElementsByClassName('modal');
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
+
+// ** Open URLs on click ** //
+function openInNewTab(url) {
+  window.open(url, '_blank').focus();
+}
+
+function navigateToURL(url) {
+  window.location.href = url;
+}
